e6a91af2c11f7cd1b0da00603411e2773e0a6008
rjgcooper <rjgcooper@users.noreply.github.com>
2026-01-08 12:52:54 +1100

    FPS tick

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    g_clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -14,6 +16,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = g_clock.tick(60) / 1000
+        #print(f"{dt}")
         
 
 if __name__ == "__main__":
